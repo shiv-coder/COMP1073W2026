005cc9a54bd1fa9adc9987e528aba817ae826f28
shiv-coder <shivi359@gmail.com>
2026-01-13 11:03:12 -0500

    completed string

--- lesson-2/js/strings.js
+++ lesson-2/js/strings.js
@@ -1,20 +1,31 @@
 const output = document.querySelector('p');
     
     /* STEP 1: Creating Strings */
-
+        let step1a = 'This is "a" string';
+        output.textContent = step1a;
+        let step1b = "This is a string";
+         output.textContent = step1b;
     
     // Note - whether you use 'single' or "double" quotes is a personal preference - try to be consistent
 
     /* STEP 2: Escaping Characters */
-
+    let step2a = 'It\'s six o\' clock';
+    output.textContent = step2a;
 
     /* STEP 3: Concatenation */
-
+    let step3a = 'Javascript';
+    let step3b = 'rocks';
+    output.textContent = step3a + ' ' + step3b;
 
     /* STEP 4: Numbers and Strings */
+    let step4a = 'Faranheit' + 543;
+    output.textContent = step4a + ' ' + typeof(step4a);
 
-    // numbers can be converted to strings
+      // strings can be converted to numbers, too
+ let step4b = Number('20') + Number('18');
+    output.textContent = step4b + ' ' + typeof(step4b);
 
-    // strings can be converted to numbers, too
-
+      // numbers can be converted to strings
+   let step4c = step4b.toString();
+    output.textContent = step4c + ' ' + typeof(step4c);;
     // and back again, if we want
