9018cc5c2ca37ad27151f971a69ed92030e44639
shiv-coder <shivi359@gmail.com>
2026-01-20 10:27:05 -0500

    completed Array methods

--- lesson-3/js/arrays.js
+++ lesson-3/js/arrays.js
@@ -32,28 +32,68 @@ myArray.forEach(function(item){
     output.textContent += ', ' + item;
 });
 
+//Arrow function:is a shotery way to write a function in JS
+/*
+
+(paramter) => {
+    //code
+    }
+ */
+//ForEach Using Arrow function
+myArray.forEach(item =>{
+console.log(item);
+    output.textContent += ', ' + item;
+});
+
+
+
 /* STEP 4: Convert a string to an array
 If there is a common character that can act as a delimiter in a string, we can use this character to create an array */
 
+let origFive = 'Toronto Maple Leafs, Chicago Black Hawks, Detroiy Red Wings, Boston Bruins, Montreal Canadians';
+console.log(origFive);
+
+let origFiveArray = origFive.split(', ');
+console.log(origFiveArray);
+
 // Output one of the array items
+output.textContent = origFiveArray[2];
 
 // Output the last element of the array
+output.textContent = origFiveArray[origFiveArray.length-1]
 
 /* STEP 5: Convert an array back to a string
 Use join() and toString() - note that join() allows you to choose and insert a delimiter, while toString() does not */
 
+let origFiveString = origFiveArray.join('-');
+output.textContent = origFiveString;
+
+
+let origFiveString2 = origFiveArray.toString();
+output.textContent = origFiveString2;
 /* STEP 6: Adding and removing items from an array
 Without the ability to edit the contents of an array, this type of variable would have limited use - but adding and removing array items is pretty straightforward */
 
 // Adding one or more items to an array with push()
+let origSevenArray = origFiveArray.push('Network Rangers','Philadelphia Flyers');
+output.textContent = origSevenArray;
 
 // If you would like to capture how many elements are in the array after you have edited it, then…
-
+ 
+output.textContent = origSevenArray + ' items and they are ' + origFiveArray;
 // Removing an item from an array with pop()
+output.textContent = origFiveArray.pop();
 
 // pop() returns the item that was removed, rather than the length of the updated array, so…
 
 // To do the same thing, that is, to add and remove an item from the beginning of the array, use shift() and unshift()
+origSevenArray = origFiveArray.unshift('Buffalo Sabres');
+output.textContent = origSevenArray;
+output.textContent = origFiveArray;
+
+
+output.textContent = origFiveArray.shift();
+output.textContent = origFiveArray;
 
 // We can also modify the array contents by deleting or substituting elements, or inserting one or more elements at a certain place with splice()
 
