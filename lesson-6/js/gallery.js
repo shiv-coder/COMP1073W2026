aee30b3ef7e4d3a9a33e844ea5e5e35e2181f4f8
shiv-coder <shivi359@gmail.com>
2026-02-10 10:54:05 -0500

    completed gallery file

--- lesson-6/js/gallery.js
+++ lesson-6/js/gallery.js
@@ -1,25 +1,43 @@
 // STEP 1: Initialize and declare variables
+const displayedImg = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
 
 
 /* STEP 2: Loop 5 times to create the <img> elements */
-
+for(let i=1;i<=5;i++){
 		/* STEP 3a: Create a new DOM node - an image element */
-
+	const newImage = document.createElement('img');
 		/* STEP 3b: Set the src attribute to be the path of one of the images inside the images folder, using setAttribute() */
-
+		newImage.setAttribute('src',`images/pic${i}.jpg`);
 		/* Append the new image element to the thumbBar div, named in STEP 1 */
-
+		thumbBar.appendChild(newImage);
 		/* STEP 3c: Build event handler for each <img> */
+		// newImage.addEventListener('click',function(event){
+		// 	let imgSrc = event.target.getAttribute('src');
+		// 	console.log(imgSrc);
+		// 	displayImage(imgSrc);
+		// })
 
-
+}
 /* STEP 4: Function to change the src of the main <img> */
-
+function displayImage(value){
 	// Rewrite the src attribute of the .displayed-img element
+		displayedImg.setAttribute('src',value);
+			
+
+}
 
 
 /* STEP 5: Event Delegation
 Instead of adding event handlers for each image, how about event delegation? Build a click handler on the parent element, and capture each target (and its attributes) from the event object */
+thumbBar.addEventListener('click',function(event){
+	if(event.target && event.target.nodeName === 'IMG'){
+		let imgSrc = event.target.getAttribute('src');
+			console.log(imgSrc);
+			displayImage(imgSrc);
 
+	}
+})
 	// event.target is the element that was clicked
 
 		// grab the src attribute of the element that was clicked
