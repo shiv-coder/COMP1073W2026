183789ee3c09c3e0ad8be486f8640bdfcc6ed5ec
shiv-coder <shivi359@gmail.com>
2026-03-10 09:46:16 -0400

    completed index file

--- lesson-9/js/index.js
+++ lesson-9/js/index.js
@@ -8,7 +8,11 @@ const output = document.querySelector('#output');
    - genre
    - players
 ========================================= */
-
+function Game(title,genre,players){
+   this.title = title;
+   this.genre = genre;
+   this.players = players;
+}
 
 
 
@@ -18,13 +22,17 @@ const output = document.querySelector('#output');
    description() that returns game info.
 ========================================= */
 
-
+Game.prototype.description = function(){
+   return `${this.title} is a ${this.genre} game with ${this.player} players online`;
+}
 
 
 /* =========================================
    STEP 3:
    Instantiate a game object using new Game()
 ========================================= */
+
+const game1 = new Game("Battle Arena","Action",1200);
 
 
 
@@ -33,7 +41,7 @@ const output = document.querySelector('#output');
    STEP 4:
    Display description() result in output
 ========================================= */
-
+output.textContent = game1.description();
 
 
 
@@ -42,23 +50,22 @@ const output = document.querySelector('#output');
    Create another object using Object.create()
    based on first game.
 ========================================= */
-
-
-
+const game2 = Object.create(game1);
 
 /* =========================================
    STEP 6:
    Modify properties of new object
 ========================================= */
-
-
-
+game2.title = "Space Battle";
+game2.genre = "Sci-fi";
+game2.players = 800;
 
 /* =========================================
    STEP 7:
    Use constructor to create another instance
 ========================================= */
-
+const game3 = new game1.constructor("Fanatsy Quest","RPG",500);
+//game1.constructor === Game
 
 
 
@@ -66,3 +73,6 @@ const output = document.querySelector('#output');
    STEP 8:
    Add prototype method isPopular()
 ========================================= */
+Game.prototype.isPopular = function(){
+   return this.players > 1000 ? "This game is very popular" : "This game has a small community";
+}
