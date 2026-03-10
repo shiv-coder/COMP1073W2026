4602e7f35993d360bc38a9811adebb7e82cfd8f2
shiv-coder <shivi359@gmail.com>
2026-03-10 09:13:05 -0400

    Added a new folder-Class

---
+++ lesson-9/js/index.js
@@ -0,0 +1,68 @@
+const output = document.querySelector('#output');
+
+/* =========================================
+   STEP 1:
+   Create a constructor function called Game
+   It should store:
+   - title
+   - genre
+   - players
+========================================= */
+
+
+
+
+/* =========================================
+   STEP 2:
+   Add a method to the prototype called
+   description() that returns game info.
+========================================= */
+
+
+
+
+/* =========================================
+   STEP 3:
+   Instantiate a game object using new Game()
+========================================= */
+
+
+
+
+/* =========================================
+   STEP 4:
+   Display description() result in output
+========================================= */
+
+
+
+
+/* =========================================
+   STEP 5:
+   Create another object using Object.create()
+   based on first game.
+========================================= */
+
+
+
+
+/* =========================================
+   STEP 6:
+   Modify properties of new object
+========================================= */
+
+
+
+
+/* =========================================
+   STEP 7:
+   Use constructor to create another instance
+========================================= */
+
+
+
+
+/* =========================================
+   STEP 8:
+   Add prototype method isPopular()
+========================================= */
---
+++ lesson-9/js/multiplayerGame.js
@@ -0,0 +1,79 @@
+const output = document.querySelector('#output');
+
+
+/* STEP 1:
+Create a JavaScript class called Game.
+The class should store the title, genre, and maxPlayers.
+*/
+
+
+
+
+/* STEP 2:
+Instantiate a new Game object using the class above.
+Call it firstGame.
+*/
+
+
+
+
+/* STEP 3:
+Add a method to the Game class called gameInfo()
+that returns a description of the game.
+*/
+
+
+
+
+/* STEP 4:
+Call the gameInfo() method and display the result
+in the output paragraph.
+*/
+
+
+
+
+/* STEP 5:
+Create a subclass called MultiplayerGame that extends Game.
+Add a new property called serverRegion.
+*/
+
+
+
+
+/* STEP 6:
+Inside MultiplayerGame, add a method called serverInfo()
+that describes which server the game is running on.
+*/
+
+
+
+
+/* STEP 7:
+Create an instance of MultiplayerGame called battleRoyale.
+*/
+
+
+
+
+/* STEP 8:
+Call the serverInfo() method and display it in the console.
+*/
+
+
+
+
+/* STEP 9:
+In the console, create another MultiplayerGame object
+and test both methods:
+- gameInfo()
+- serverInfo()
+*/
+
+
+
+
+/* STEP 10:
+Add another method to MultiplayerGame called isPopular()
+that returns a message depending on the number of players.
+*/
